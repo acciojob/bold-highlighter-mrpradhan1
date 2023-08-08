1b6574106aacb2754955d9bed3eda889f26d5647
Manoranjan Pradhan <133505638+mrpradhan1@users.noreply.github.com>
2023-08-08 18:15:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function highlight() {
     //Write your code here
-
+	let green = document.getElementsByTagName('strong');
+	for( const strong of green){
+		strong.style.color='green';
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let black = document.getElementsByTagName('strong');
+	for( const strong of black){
+		strong.style.color='black';
+	}
     
 }
